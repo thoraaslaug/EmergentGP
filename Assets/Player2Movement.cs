25f761a85f70675b34467e4f8f1ce91417a66af2
Thora Magnusdottir <70522885+thoraaslaug@users.noreply.github.com>
2024-02-13 15:30:25 +0100

    want to die

--- Assets/Player2Movement.cs
+++ Assets/Player2Movement.cs
@@ -8,21 +8,57 @@ public class Player2Movement : MonoBehaviour
     float horizontalMove = 0f;
     bool jump = false;
     bool crouch = false;
+    public float freezeDuration = 1f;
+    private bool isFrozen = false;
+    private float freezeTimer = 0f;
 
     void Update()
     {
         // Player 2 movement with arrow keys
         horizontalMove = Input.GetAxisRaw("Horizontal2") * runSpeed;
-
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+       
+        // Update freeze timer if frozen
+        if (isFrozen)
         {
-            jump = true;
+            freezeTimer -= Time.deltaTime;
+            if (freezeTimer <= 0f)
+            {
+                isFrozen = false;
+                controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+            }
+            else
+            {
+                jump = false;
+            }
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                jump = true;
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Trail2"))
+        {
+            // Freeze the character
+            Debug.Log("Collided with trail");
+            isFrozen = true;
+            freezeTimer = freezeDuration;
+            // Stop character movement here 
         }
     }
 
     void FixedUpdate()
     {
-        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
-        jump = false;
+        if (!isFrozen)
+        {
+            // Only move the character if not frozen
+            controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+            jump = false;
+        }
     }
 }
--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -8,20 +8,57 @@ public class PlayerMovement : MonoBehaviour
     float horizontalMove = 0f;
     bool jump = false;
     bool crouch = false;
+    public float freezeDuration = 5f; // Adjusted freeze duration to 5 seconds
+    private bool isFrozen = false;
+    private float freezeTimer = 0f;
 
     void Update()
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
         // Update freeze timer if frozen
-        if (Input.GetButtonDown("Jump"))
+        if (isFrozen)
         {
-            jump = true;
+            freezeTimer -= Time.deltaTime;
+            if (freezeTimer <= 0f)
+            {
+                isFrozen = false;
+                controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+            }
+            else
+            {
+                jump = false;
+            }
+        }
+        else
+        {
+            // If not frozen, allow normal movement
+            if (Input.GetButtonDown("Jump"))
+            {
+                jump = true;
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Trail"))
+        {
+            // Freeze the character
+            Debug.Log("Collided with trail");
+            isFrozen = true;
+            freezeTimer = freezeDuration;
+            // Stop character movement here
         }
     }
 
     void FixedUpdate()
+    {
+        if (!isFrozen)
         {
+            // Only move the character if not frozen
             controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
             jump = false;
         }
     }
+}
